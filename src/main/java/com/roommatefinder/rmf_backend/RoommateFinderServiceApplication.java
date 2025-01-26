82c73eb987c08c16d7ddf5628f6bc3847c0cf435
Jitendra Tiwari <jkjitendra725@gmail.com>
2025-01-26 23:52:40 +0530

    initial commit

---
+++ src/main/java/com/roommatefinder/rmf_backend/RoommateFinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roommatefinder.rmf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateFinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoommateFinderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roommatefinder/rmf_backend/RoommateFinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roommatefinder.rmf_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommateFinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
